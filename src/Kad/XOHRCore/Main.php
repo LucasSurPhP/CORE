56f799c8fe47e356dba0471cce6ba407fd925db0
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-11-28 18:46:09 -0500

    Don't have /nv, but everything else is fixed

--- src/Kad/XOHRCore/Main.php
+++ src/Kad/XOHRCore/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Kad\XOHRCore;
 
 use pocketmine\plugin\PluginBase;
@@ -23,27 +24,38 @@ class Main extends PluginBase implements Listener{
     public fts = "§7[§dX§aO§dX§aO§7]§r;
     
     public function onEnable() {
-    // Still need to redo this whole area
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $name = $player->getName();
-     #  $event->setJoinMessage("");
-     #  $player->teleport($hub);
+        $event->setJoinMessage("§0• §7[§b+§7]§f" . $name");
+        $world = $this->getServer()->getLevelByName("world");
+        $x = 210.5;
+        $y = 68;
+        $z = 90.5;
+        $pos = new Position($x, $y, $z, $world);
+        $player->teleport($pos);
         $player->setGamemode(1);
     }   
     public function onQuit(PlayerQuitEvent $event) {
         $player = $event->getPlayer();
         $name = $player->getName();
-     #  $event->setQuitMessage();
+        $event->setQuitMessage("§0• §7[§b-§7]§f" . $name");
     }
     public function onDeath(PlayerDeathEvent $event) {
         $player = $event->getPlayer();
         $name = $player->getName();
-    #   $event->setDeathMessage("");
+        $event->setDeathMessage("§0• §7[§cX§7]§f" . $name");
     }
     public function onRespawn(PlayerRespawnEvent $event) {
-        $event->setRespawnPosition($hub);
+        $world = $this->getServer()->getLevelByName("world");
+        $x = 210.5;
+        $y = 68;
+        $z = 90.5;
+        $pos = new Position($x, $y, $z, $world);
+        $player->teleport($pos);
+        $player->setGamemode(1);
     }
     /**
      * @param LeavesDecayEvent $event
@@ -54,81 +66,90 @@ class Main extends PluginBase implements Listener{
     }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
     {
-        if(!$sender instanceof Player) return true;
-        switch($cmd->getName()) {
-                case "gmc":
+        if($cmd->getName() == "gmc") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.gmc.use")) {
-                        $sender->setGamemode(1);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                    $sender->setGamemode(1);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");    
                 }
-                break;
-                case "gms":
+            }
+        }
+        if($cmd->getName() == "gms") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.gms.use")) {
-                         $sender->setGamemode(0);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                    $sender->setGamemode(0);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Survival!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
                 }
-                break;
-                case "gma":
+            }
+        }
+        if($cmd->getName() == "gma") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.gma.use")) {
-                        $sender->setGamemode(2);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                    $sender->setGamemode(2);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Adventure!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
                 }
-                break;
-                case "gmspc":
+            }
+        }
+        if($cmd->getName() == "gmspc") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.gmspc.use")) {
-                        $sender->setGamemode(3);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                    $sender->setGamemode(3);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Spectator!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
                 }
-                break;
-                case "day":
+            }
+        }
+        if($cmd->getName() == "day") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.day.use")) {
-                        $sender->getLevel()->setTime(6000);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Day (6000) in your world!");
+                    $sender->getLevel()->setTime(6000);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Day (6000) in your world!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
                 }
-                break;
-                case "night":
+            }
+        }
+        if($cmd->getName() == "night") {
+            if($sender instanceof Player) {
                 if($sender->hasPermission("xohrcore.night.use")) {
-                        $sender->getLevel()->setTime(16000);
-                        $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Night (16000) in your world!");
+                    $sender->getLevel()->setTime(16000);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Night (16000) in your world!");
                 } else {
-                        $sender->sendMessage(str_replace("%prefix", $this->fts, $this->cfg->getNested("messages.cmd-error")));
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
                 }
-                break;
-                case "hub":
-                        $sender->teleport($this->hubPosition);
-                        $sender->sendMessage($this->fts . TF::GOLD . "Teleported to Hub");
-                break;
-                case "hybridhub": case "hhub": case "h-h":
-                        $sender->teleport($this->hhubPosition);
-                        $sender->sendMessage($this->fts . TF::GOLD . "Teleported to Hybridian Prime");
-                break;
-                case "rules":
-                        $sender->sendMessage("§6§o§lXOXO High RolePlay Rules§r");
-                        $sender->sendMessage("§f- §eNo Advertising");
-                        $sender->sendMessage("§f- §eNo NSFW");
-                        $sender->sendMessage("§f- §eNo cursing. (Censoring words is allowed.)");
-                        $sender->sendMessage("§f- §eNo asking for OP/Ranks/Perms");
-                        $sender->sendMessage("§f- §eUse Common Sense. Failure to do so will not exempt you from punishment.");
-                break;
-                case "info":
-                        $sender->sendMessage("§6§o§lXOXO High RP Rebooted Info§r");
-                        $sender->sendMessage("§eXOXO High RolePlay is a server for the remnants");
-                        $sender->sendMessage("§eof JM Pocket Creative, Nebula Games, XOXO High RolePlay, Neptune, Lapis Games, and Orion RolePlay PvP & Plots.");
-                        $sender->sendMessage("§eThere are 5 Leaders, Kad, Cara, Becca, Skull, & Chocky.");
-                        $sender->sendMessage("§eThe server is meant to bring together whoever is still there from the past, regardless of the various wars and drama that occured between them.");
-                        $sender->sendMessage("§eDiscord Link: https://discord.gg/A64ZVAa");
-                break;
-	}
-        return true;
-     }
+            }
+        }
+        if($cmd->getName() == "hub") {
+            if($sender instanceof Player) {
+                $level = $this->getServer()->getLevelByName("world");
+                $x = 210.5;
+                $y = 68;
+                $z = 90.5;
+                $pos = new Position($x, $y, $z, $level);
+                $sender->teleport($pos);
+                $sender->sendMessage($this->fts . TF::GOLD . "Teleported to Hub");
+                $sender->setGamemode(1);
+            } else {
+                $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
+            }
+        }
+        if($cmd->getName() == "rules") {
+            if($sender instanceof Player) {
+                $sender->sendMessage("§6§o§lXOXO High RolePlay Server Rules§r");
+                $sender->sendMessage("§f- §eNo Advertising");
+                $sender->sendMessage("§f- §eNo NSFW");
+                $sender->sendMessage("§f- §eNo cursing. (Censoring words is allowed.)");
+                $sender->sendMessage("§f- §eNo asking for OP/Ranks/Perms");
+                $sender->sendMessage("§f- §eUse Common Sense. Failure to do so will not exempt you from punishment.");
+            }
+        }
+    return true;
+    }
 }
