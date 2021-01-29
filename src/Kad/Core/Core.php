4139ba1481c3a0e3c79af06add496f19737fc8dd
Clik <47439649+LegoFan48737@users.noreply.github.com>
2021-01-29 16:55:51 -0500

    *Ha, laughed the fat PHP. You are wrong.*

--- src/Kad/Core/Core.php
+++ src/Kad/Core/Core.php
@@ -119,164 +119,163 @@ class Core extends PluginBase implements Listener{
 		Server::getInstance()->broadcastPacket($player->getLevel()->getPlayers(), $sound);
     }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args ) :bool{
-        switch(strtolower($cmd->getName())){
-            case "gms":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.gms.use")){
-                        $sender->setGamemode(0);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Survival!");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
+        if($cmd->getName() == "gms"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.gms.use")){
+					$sender->setGamemode(0);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Creative!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+		if($cmd->getName() == "gmc"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.gmc.use")){
+					$sender->setGamemode(1);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Survival!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+		if($cmd->getName() == "gma"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.gma.use")){
+					$sender->setGamemode(2);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Adventure!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+		if($cmd->getName() == "gmspc"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.gmspc.use")){
+					$sender->setGamemode(3);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Spectator!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+		if($cmd->getName() == "day"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.day.use")){
+					$sender->getLevel()->setTime(6000);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Set the time to Day (6000) in your world!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+		if($cmd->getName() == "night"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.night.use")){
+					$sender->getLevel()->setTime(16000);
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " Set the time to Night (16000) in your world!");
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+        if($cmd->getName() == "nv"){
+			if($sender instanceof Player){
+				if($sender->getEffect(Effect::NIGHT_VISION)){
+					$sender->sendMessage($this->kyt . TF::GREEN . " Night Vision turned off!");
+					$sender->removeEffect(Effect::NIGHT_VISION);
+				}else{
+					$sender->sendMessage($this->kyt . TF::GREEN . " Night Vision turned on!");
+					$sender->addEffect(new EffectInstance(Effect::getEffectByName("NIGHT_VISION"), INT32_MAX, 1, false));
+				}
+			}else{
+				$sender->sendMessage("This command only works in game");
+			}
+		}
+        if($cmd->getName() == "clearinv"){
+			if($sender instanceof Player){
+				$sender->getInventory()->clearAll();
+				$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+			}else{
+				$sender->sendMessage("Please use this command in-game.");
+			}
+		}
+        if($cmd->getName() == "tpworld"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.tpworld.use")){
+					$world = strtolower($args[0]);
+					$level = $this->getServer()->getLevelByName($world);
+					$sender->teleport($level->getSafeSpawn());
+					$sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+					$sender->sendMessage($this->kyt . TF::GREEN . " You have been teleported to " . TF::GOLD . $world);
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Sir, you just tried to teleport a non-existent entity into a virtual game to teleport them to another world in said game. I recommend you go see a psychologist.");
+			}
+		}
+        if($cmd->getName() == "ii"){
+            if($sender instanceof Player){
+                $item = $player->getItemInHand()->getId();
+                $damage = $player->getItemInHand()->getDamage();
+                $sender->sendMessage($this->kyt . TF::GREEN . " ID: " . $item . ":" . $damage);
+            }else{
+                $sender->sendMessage("Please use this command in-game.");
             }
-            return true;
-            break;
-            case "gmc":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.gmc.use")){
-                        $sender->setGamemode(1);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Creative!");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
-                }
-            return true;
-            break;
-            case "gma":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.gma.use")){
-                        $sender->setGamemode(2);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Adventure!");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
-                }
-            return true;
-            break;
-            case "gmspc":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.gmspc.use")){
-                        $sender->setGamemode(3);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Your gamemode has been set to Spectator!");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
-                }
-            return true;
-            break;
-            case "day":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.day.use")){
-                        $sender->getLevel()->setTime(6000);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Time has been set to Day (6000) in your world.");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
-                }
-            return true;
-            break;
-            case "night":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.night.use")){
-                        $sender->getLevel()->setTime(16000);
-                        $sender->getLevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Time has been set to Night (16000) in your world.");
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game.");
-                }
-            case "nv":
-                if($sender instanceof Player){
-                    if($sender->getEffect(EFFECT::NIGHT_VISION)){
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Night Vision turned off!");
-					    $sender->removeEffect(Effect::NIGHT_VISION);
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::GREEN . " Night Vision turned on!");
-					    $sender->addEffect(new EffectInstance(Effect::getEffectByName("NIGHT_VISION"), INT32_MAX, 1, false));
-                    }else{
-                        $sender->sendMessage("This command only works in game");
-                    }
-                }
-            return true;
-            break;
-            case "lightning":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.lightning.use")){
-                        $this->Lightning($sender);
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Please use this command in-game. We don't want the console fried by lightning. Idiot...");
-                }
-            return true;
-            break;
-            case "clearinv":
-                if($sender instanceof Player){
-                    $sender->getInventory()->clearAll();
-                    $sender->getevel()->addSound(new GhastShootSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
-                }else{
-                    $sender->sendMessage("As messy as the servers inventory is, maybe lets not clear it, mk?");
-                }
-            return true;
-            break;
-            case "tpworld":
-                if($sender instanceof Player){
-                    if($sender->hasPermission("core.tpworld.use"){
-                        $world = strtolower($args[0]);
-                        $level = $this->getServer()->getLevelByName($world);
-                        $sender->teleport($level->getSafeSpawn());
-                        $sender->getLevel()->addSound(new EndermanTeleportSound(new Vector3($sender->getX(), $sendr->getY(), $sender->getZ())));
-                        $sender->sendMessage($this->kyt . TF::GREEN . " You have been teleported to " . TF::GOLD . $world);
-                    }else{
-                        $sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
-                    }
-                }else{
-                    $sender->sendMessage("Sir, you just tried to teleport a non-existent entity into a virtual game to teleport them to another world in said game. I recommend you go see a psychologist.");
-                }
-            return true;
-            break;
-            # All commands after this will likely need modifications more than once.
-            case "hub":
-                if($sender instanceof Player){
-                    $sender->sendMessage($this->kyt . TF::GREEN . " This command is not ready yet :|");
-                }else{
-                    $sender->sendMessage("Sir, you just tried to teleport a non-existent entity into a virtual game to teleport them to another world in said game. I recommend you go see a psychologist.");
-                }
-            return true;
-            break;
-            case "rules":
-                if($sender instanceof Player){
-                    $sender->sendMessage("§6§o§lKYT Server Rules§r");
-				    $sender->sendMessage("§f- §eNo advertising in any way, shape or form. §c(§4Ban§c)");
-				    $sender->sendMessage("§f- §eNo NSFW/18+ Builds, Chat or Content. §c(§4Ban§c)");
-				    $sender->sendMessage("§f- §eNo asking for OP/Ranks/Perms. §c(§4Kick, then Ban§c)");
-				    $sender->sendMessage("§f- §eNo Drama. We've all had enough of it elsewhere, please do not bring it here. §c(§4Kick, then Ban§c)");
-                    $sender->sendMessage("§f- §eNo Lavacasts/Other excessive usages of Lava and Water. §c(§4Ban§c)");
-                    $sender->sendMessgae("§f- §eNo Dolphin Porn. §c(§4Ban§c)");
-				    $sender->sendMessage("§f- §eThat's it, have fun §b:)§e");
-                }else{
-                    $sender->sendMessage("If you have console access you BETTER know the fucking rules...");
-                }
-            return true;
-            break;
         }
-    }
+        if($cmd->getName() == "lightning"){
+			if($sender instanceof Player){
+				if($sender->hasPermission("core.lightning.use")){
+					$this->Lightning($sender);
+				}else{
+					$sender->sendMessage($this->kyt . TF::RED . " You do not have permission to use this command!");
+				}
+			}else{
+				$sender->sendMessage("Please run this command in-game.");
+			}
+		}
+        # All commands after this will likely need modifications more than once.
+		if($cmd->getName() == "hub"){
+			if($sender instanceof Player){
+				$sender->getLevel()->addSound(new EndermanTeleportSound(new Vector3($sender->getX(), $sender->getY(), $sender->getZ())));
+				$sender->sendMessage($this->kyt . TF::GOLD . " Teleported to Hub");
+			}else{
+				$sender->sendMessage("Sir, you just tried to teleport a non-existent entity into a virtual game to teleport them to another world in said game. I recommend you go see a psychologist.");
+			}
+		}
+		if($cmd->getName() == "rules"){
+			if($sender instanceof Player){
+				$sender->sendMessage("§6§o§lKYT Server Rules§r");
+				$sender->sendMessage("§f- §eNo advertising in any way, shape or form. §c(§4Ban§c)");
+			    $sender->sendMessage("§f- §eNo NSFW/18+ Builds, Chat or Content. §c(§4Ban§c)");
+			    $sender->sendMessage("§f- §eNo asking for OP/Ranks/Perms. §c(§4Kick, then Ban§c)");
+			    $sender->sendMessage("§f- §eNo Drama. We've all had enough of it elsewhere, please do not bring it here. §c(§4Kick, then Ban§c)");
+                $sender->sendMessage("§f- §eNo Lavacasts/Other excessive usages of Lava and Water. §c(§4Ban§c)");
+                $sender->sendMessgae("§f- §eNo Dolphin Porn. §c(§4Ban§c)");
+			    $sender->sendMessage("§f- §eThat's it, have fun §b:)§e");
+			}else{
+				$sender->sendMessage("If you have console access you BETTER know the fucking rules...");
+			}
+		}
+		return true;
+	}
 }
