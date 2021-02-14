37537b26e0dae651db6e62d89a0606f94c1448e9
Clik <47439649+LegoFan48737@users.noreply.github.com>
2021-02-14 12:24:48 -0500

    Add BroadcastTask (version bump 5.4.0)

---
+++ src/Core/Tasks/BroadcastTask.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Tasks;
+
+use pocketmine\scheduler\Task;
+use pocketmine\util\TextFormat as TF;
+
+use Core\Core;
+
+class BroadcastTask extends Task{
+
+    /** @var Core $plugin */
+    private $plugin;
+
+    /** @var int $i */
+    private $i;
+
+    public function __construct(Core $plugin){
+        $this->plugin = $plugin;
+            $this->i = 0;
+    }
+    public function onRun(int $currentTick){
+        $messages = $this->plugin->config["message-broadcast"]["messages"];
+        back:
+        if($this->i < count($messages)){
+            $this->plugin->getServer()->broadcastMessage(TF::colorize($this->plugin->formatMessage($messages[$this->i])));
+            $this->i++;
+        }else{
+            $this->i = 0;
+            goto back;
+        }
+    }
+}
